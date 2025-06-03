398a8aec16942b8598613ade90ef972bf32b0815
Sakhee12 <157628964+Sakhee12@users.noreply.github.com>
2025-06-03 22:19:44 +0530

    Add files via upload

---
+++ Pattern2.java
@@ -0,0 +1,17 @@
+//Rectangle Pattern
+
+class Pattern2 
+{
+	public static void main(String[] args) 
+	{
+		for(int i=0;i<3;i++)//length of the rectangle
+		{
+			for(int j=0;j<4;j++)//breadth of rec
+			{
+				System.out.print("*"+" ");
+			}
+			System.out.println(" ");
+		}
+		
+	}
+}
