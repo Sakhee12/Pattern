66e8d11496f0fd5574606e249b7ec0bf0f33cb8a
Sakhee12 <157628964+Sakhee12@users.noreply.github.com>
2025-06-03 22:20:04 +0530

    Add files via upload

---
+++ Pattern3.java
@@ -0,0 +1,16 @@
+class Pattern3 
+{
+	public static void main(String[] args) 
+	{
+		int n=4;
+		for(int j=0;j<n;j++)
+		{
+			for(int i=0;i<n-1;i++)
+			{
+				System.out.print("*"+" ");
+			}
+			System.out.println(" ");
+		}
+		
+	}
+}
