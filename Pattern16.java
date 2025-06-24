6a7b18849202875a29409d516c23b0b023e5c4b9
Sakhee12 <157628964+Sakhee12@users.noreply.github.com>
2025-06-24 23:15:12 +0530

    Add files via upload

---
+++ Pattern16.java
@@ -0,0 +1,43 @@
+/*
+* * * * 
+  * * *
+    * *
+	  *
+
+	  */
+
+class Pattern16 
+{
+	public static void main(String[] args) 
+	{
+		/*
+		for(int i=1;i<5;i++)
+		{
+			for(int j=1;j<i;j++)
+			{
+				System.out.print(" ");
+			}
+			for(int k=1;k<(5-j);k++)
+			{
+				System.out.print("*");
+			}
+	    System.out.println("");
+		}
+		*/
+        for(int i=1;i<5;i++)
+		{
+			for(int j=1;j<5;j++)
+			{
+				if(j<i)
+				{
+					System.out.print("  ");
+				}
+				else
+				{
+					System.out.print("* ");
+				}
+			}
+	    System.out.println();
+		}
+	}
+}
